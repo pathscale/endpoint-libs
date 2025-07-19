b6d65ca4798cc4d811ea0c668c3564aecd8f0273
apakhomov <apkhmv@gmail.com>
2025-07-19 15:15:41 +0300

    [&u32] -> [u32]

--- src/libs/ws/server.rs
+++ src/libs/ws/server.rs
@@ -104,17 +104,17 @@ impl WebsocketServer {
     }
     pub fn add_handler<T: RequestHandler + 'static>(&mut self, handler: T) {
         let schema = serde_json::from_str(T::Request::SCHEMA).expect("Invalid schema");
-        let roles: &[&u32] = T::Request::ROLES;
+        let roles: &[u32] = T::Request::ROLES;
         check_handler::<T>(&schema).expect("Invalid handler");
         self.add_handler_erased(schema, roles, Arc::new(handler))
     }
     pub fn add_handler_erased(
         &mut self,
         schema: EndpointSchema,
-        roles: &[&u32],
+        roles: &[u32],
         handler: Arc<dyn RequestHandlerErased>,
     ) {
-        let roles_set = Some(roles.iter().map(|&x| *x).collect::<HashSet<u32>>());
+        let roles_set = Some(roles.iter().map(|&x| x).collect::<HashSet<u32>>());
         let _old_roles = self.allowed_roles.insert(schema.code.clone(), roles_set);
 
         let old = self
